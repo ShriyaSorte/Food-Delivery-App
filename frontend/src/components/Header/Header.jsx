9813cac1293458dc8ea27ba17da48a7ca1a5c69b
Shriya Sorte <shriyasorte2000@gmail.com>
2024-08-07 14:53:22 +0530

    changes in codes

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your favourite food here!!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptas corrupti. Harum, cum! Officiis perspiciatis sed adipisci harum dolores, quos voluptate reprehenderit est nihil odio.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
