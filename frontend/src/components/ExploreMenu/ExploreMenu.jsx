f1c2adb2c39750508af18892694cb35c57a7cdc7
Shriya Sorte <shriyasorte2000@gmail.com>
2024-08-09 12:24:03 +0530

    menu code

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import './ExploreMenu.css';
 import { menu_list } from '../../assets/assets';
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
+
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu..</h1>
-      <p className='explore-menu-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolor excepturi nesciunt accusamus quis dolorem odit vel, expedita illum in?</p>
+      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience , one delicious meal at a time.</p>
       <div className="explore-menu-list">
         {menu_list.map((item, index)=>{
           return(
-            <div key={index} className='explore-menu-list-item'>
-              <img src={item.menu_image} alt=''/>
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt=''/>
               <p>{item.menu_name}</p>
             </div>
           )
